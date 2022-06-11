d5a1baf6c85396ef27742a294110521509fba26c
Shaho Bashoki <bashoki.shaho@gmail.com>
2022-06-11 15:20:04 +0430

    21th commit

--- util/redpanda_test.go
+++ util/redpanda_test.go
@@ -13,7 +13,6 @@ func TestRedpandaSend(t *testing.T) {
 	defer server.Close()
 
 	NewConfig(context.Background(), &TestLogger{})
-	ModuleConfig.RedpandaURL = fmt.Sprintf("%s/topics/nakama", server.URL)
 
 	type args struct {
 		ctx     context.Context
@@ -24,6 +23,7 @@ func TestRedpandaSend(t *testing.T) {
 		name    string
 		args    args
 		wantErr bool
+		init    func()
 	}{
 		{
 			name: "RedpandaSend",
@@ -33,10 +33,49 @@ func TestRedpandaSend(t *testing.T) {
 				payload: map[string]interface{}{},
 			},
 			wantErr: false,
+			init: func() {
+				ModuleConfig.RedpandaURL = fmt.Sprintf("%s/topics/nakama", server.URL)
+			},
+		},
+		{
+			name: "RedpandaSendWithError",
+			args: args{
+				ctx:     context.Background(),
+				logger:  &TestLogger{},
+				payload: map[string]interface{}{"error": func() {}},
+			},
+			wantErr: true,
+			init: func() {
+			},
+		},
+		{
+			name: "RedpandaSendWithError2",
+			args: args{
+				ctx:     context.Background(),
+				logger:  &TestLogger{},
+				payload: map[string]interface{}{},
+			},
+			wantErr: true,
+			init: func() {
+				ModuleConfig.RedpandaURL = fmt.Sprintf(":%s/topics/nakama", server.URL)
+			},
+		},
+		{
+			name: "RedpandaSendWithError3",
+			args: args{
+				ctx:     context.Background(),
+				logger:  &TestLogger{},
+				payload: map[string]interface{}{},
+			},
+			wantErr: true,
+			init: func() {
+				ModuleConfig.RedpandaURL = "*"
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			tt.init()
 			if err := RedpandaSend(tt.args.ctx, tt.args.logger, tt.args.payload); (err != nil) != tt.wantErr {
 				t.Errorf("RedpandaSend() error = %v, wantErr %v", err, tt.wantErr)
 			}
